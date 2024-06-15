44dc1c3edf8ecf05838e7d5c3f80277d47835180
sekiguchi0731 <seki.guchi.3725@gmail.com>
2024-06-15 20:21:38 +0900

    2-optの改善

--- lec05/google-step-tsp/solver_2opt.py
+++ lec05/google-step-tsp/solver_2opt.py
@@ -1,15 +1,16 @@
 import sys
 
-from common import print_tour, read_input, read_output, write_tour
+from common import read_input, read_output, write_tour
 from solver_greedy import distance
 
 
 def solve(city_dists: list[tuple[float, float]], cities_indexes: list[int]) -> list[int]:
     n: int = len(cities_indexes)
     cities_indexes.append(cities_indexes[0])
-    new_cities: list[int] = cities_indexes
-    for num in range(10000):
-        improvement: bool = False
+    new_cities: list[int] = cities_indexes[:]
+    improvement: bool = True
+    while improvement:
+        improvement = False
         for i in range(n - 1):
             for j in range(i + 2, n):
                 # 両端は変化させない
@@ -19,36 +20,31 @@ def solve(city_dists: list[tuple[float, float]], cities_indexes: list[int]) -> l
                 # 検査するインデックスの代入
                 i_idx, i_next_idx = new_cities[i], new_cities[i + 1]
                 j_idx, j_next_idx = new_cities[j], new_cities[j + 1]
-                print("検査するi:" + str(i_idx))
-                print("検査するj:" + str(j_idx))
+
                 current_sum: float = (
                     distance(city_dists[i_idx], city_dists[i_next_idx])
                     + distance(city_dists[j_idx], city_dists[j_next_idx])
                 )
-                
                 # i-j, i+1-j+1の距離計算
                 new_sum1: float = (
                     distance(city_dists[i_idx], city_dists[j_idx])
                     + distance(city_dists[i_next_idx], city_dists[j_next_idx])
                 )
-                # i-j+1, i+1-jの距離計算
-                new_sum2: float = (
-                    distance(city_dists[i_idx], city_dists[j_next_idx])
-                    + distance(city_dists[i_next_idx], city_dists[j_idx])
-                )
-                # 現在の距離が新しい距離のいずれかより大きいとき、改善する
-                if min(new_sum1, new_sum2) < current_sum:
+                print(f'今の距離：{current_sum} 更新後の距離:{new_sum1}')
+                # 現在の距離が新しい距離より大きいとき、改善する
+                if new_sum1 < current_sum:
+                    print(f"検査しているインデックス： {i_idx}, {j_idx}")
+                    print(new_cities)
                     improvement = True
-                    if new_sum1 < new_sum2:
-                        new_cities[i + 1 : j + 1] = reversed(new_cities[i + 1 : j + 1])
-                    else:
-                        new_cities[i : j + 1] = reversed(new_cities[i : j + 1])
-
+                    new_cities[i + 1 : j + 1] = reversed(new_cities[i + 1 : j + 1])
+                    print(new_cities)
+                    break
+            if improvement:
+                break
         if not improvement:
-            print(str(num) + "で終わった")
             break
         else:
-            cities_indexes = new_cities
+            cities_indexes = new_cities[:]
 
     return cities_indexes[:-1]
 
@@ -56,6 +52,5 @@ def solve(city_dists: list[tuple[float, float]], cities_indexes: list[int]) -> l
 if __name__ == "__main__":
     assert len(sys.argv) > 3
     tour: list[int] = solve(read_input(sys.argv[1]), read_output(sys.argv[2]))
-    # print_tour(tour)
     print(tour)
     write_tour(sys.argv[3], tour)
